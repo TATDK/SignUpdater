02b9f1f6f8810bcf2e176a105126daa2381c8c64
TAT <TAT@plugcubed.net>
2014-05-28 02:55:19 +0200

    Update to CraftBukkit 1.7.9-R0.2

--- src/main/java/dk/earthgame/TAT/SignUpdater/SignUpdater.java
+++ src/main/java/dk/earthgame/TAT/SignUpdater/SignUpdater.java
@@ -1,27 +1,22 @@
 package dk.earthgame.TAT.SignUpdater;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import org.bukkit.block.Sign;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.logging.Logger;
 
-import org.bukkit.block.Sign;
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.config.Configuration;
-
 /**
  * Plugin used by other plugins for updating signs
+ *
  * @author TAT
  */
 public class SignUpdater extends JavaPlugin {
     protected final Logger log = Logger.getLogger("Minecraft");
-    Configuration config;
     PluginDescriptionFile pdfFile;
-    SignUpdater signUpdater = this;
+    public SignUpdater instance = null;
     boolean output;
     //If the updater is running
     boolean running;
@@ -29,16 +24,19 @@ public class SignUpdater extends JavaPlugin {
     Runner runner = new Runner();
     //Update containers
     HashMap<Sign, String[]> Highest = new HashMap<Sign, String[]>();
-    HashMap<Sign, String[]> High    = new HashMap<Sign, String[]>();
-    HashMap<Sign, String[]> Normal  = new HashMap<Sign, String[]>();
-    HashMap<Sign, String[]> Low     = new HashMap<Sign, String[]>();
-    HashMap<Sign, String[]> Lowest  = new HashMap<Sign, String[]>();
+    HashMap<Sign, String[]> High = new HashMap<Sign, String[]>();
+    HashMap<Sign, String[]> Normal = new HashMap<Sign, String[]>();
+    HashMap<Sign, String[]> Low = new HashMap<Sign, String[]>();
+    HashMap<Sign, String[]> Lowest = new HashMap<Sign, String[]>();
 
     @Override
     public void onEnable() {
+        if (instance == null)
+            instance = this;
         pdfFile = getDescription();
-        CreateConfig();
-        LoadConfig();
+        this.saveDefaultConfig();
+
+        output = this.getConfig().getBoolean("Output", false);
 
         if (output)
             log.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
@@ -52,9 +50,10 @@ public class SignUpdater extends JavaPlugin {
 
     /**
      * Add sign to the updater
+     *
      * @param priority Priority for the update (Highest first)
-     * @param sign Sign that needs to be updated
-     * @param lines New lines on sign (Require all 4 lines)
+     * @param sign     Sign that needs to be updated
+     * @param lines    New lines on sign (Require all 4 lines)
      */
     public void AddSignUpdate(UpdaterPriority priority, Sign sign, String[] lines) {
         if (lines.length < 4) {
@@ -90,6 +89,7 @@ public class SignUpdater extends JavaPlugin {
 
     /**
      * Get the next sign in queue
+     *
      * @return Sign and new lines if found (else null)
      */
     Entry<Sign, String[]> getNext() {
@@ -108,25 +108,27 @@ public class SignUpdater extends JavaPlugin {
 
     /**
      * Get priority of sign
-     * @param s Sign
+     *
+     * @param sign Sign
      * @return Priority of sign if found (else null)
      */
-    UpdaterPriority getPriority(Sign s) {
-        if (Highest.containsKey(s))
+    UpdaterPriority getPriority(Sign sign) {
+        if (Highest.containsKey(sign))
             return UpdaterPriority.HIGHEST;
-        if (High.containsKey(s))
+        if (High.containsKey(sign))
             return UpdaterPriority.HIGH;
-        if (Normal.containsKey(s))
+        if (Normal.containsKey(sign))
             return UpdaterPriority.NORMAL;
-        if (Low.containsKey(s))
+        if (Low.containsKey(sign))
             return UpdaterPriority.LOW;
-        if (Lowest.containsKey(s))
+        if (Lowest.containsKey(sign))
             return UpdaterPriority.LOWEST;
         return null;
     }
 
     /**
      * Remove sign from queue
+     *
      * @param s Sign
      */
     void removeSign(Sign s) {
@@ -144,6 +146,7 @@ public class SignUpdater extends JavaPlugin {
 
     /**
      * Sign updating process
+     *
      * @author TAT
      */
     class Runner implements Runnable {
@@ -163,59 +166,9 @@ public class SignUpdater extends JavaPlugin {
             removeSign(s);
 
             if (getNext() != null)
-                signUpdater.getServer().getScheduler().scheduleSyncDelayedTask(signUpdater, runner, 2);
+                instance.getServer().getScheduler().scheduleSyncDelayedTask(instance, runner, 2);
             else
                 running = false;
-        }
-    }
-
-    /**
-     * Load configuration
-     */
-    void LoadConfig() {
-        config = new Configuration(new File(getDataFolder(), "config.yml"));
-        config.load();
-
-        output = config.getBoolean("Output", false);
-
-        if (output)
-            log.info(pdfFile.getName() + " - Config loaded");
-    }
-
-    /**
-     * Create configuration if not found
-     */
-    void CreateConfig() {
-        getDataFolder().mkdir();
-        String name = "config.yml";
-        File actual = new File(getDataFolder(), name);
-        if (!actual.exists()) {
-            InputStream input = SignUpdater.class.getResourceAsStream("/Config/" + name);
-            if (input != null) {
-                FileOutputStream output = null;
-
-                try {
-                    output = new FileOutputStream(actual);
-                    byte[] buf = new byte[8192];
-                    int length = 0;
-                    while ((length = input.read(buf)) > 0) {
-                        output.write(buf, 0, length);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    log.warning(pdfFile.getName() + " - Error creating config file!");
-                } finally {
-                    try {
-                        if (input != null)
-                            input.close();
-                    } catch (IOException e) {}
-
-                    try {
-                        if (output != null)
-                            output.close();
-                    } catch (IOException e) {}
-                }
-            }
         }
     }
 }
--- src/main/java/dk/earthgame/TAT/SignUpdater/UpdaterPriority.java
+++ src/main/java/dk/earthgame/TAT/SignUpdater/UpdaterPriority.java
@@ -2,12 +2,13 @@ package dk.earthgame.TAT.SignUpdater;
 
 /**
  * The priorities for updating signs in SignUpdater
+ *
  * @author TAT
  */
 public enum UpdaterPriority {
-	HIGHEST,
-	HIGH,
-	NORMAL,
-	LOW,
-	LOWEST;
+    HIGHEST,
+    HIGH,
+    NORMAL,
+    LOW,
+    LOWEST
 }
